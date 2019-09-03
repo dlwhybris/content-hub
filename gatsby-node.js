2a7b92d6e1c2b27ea587881d4cb3646e8bdd744a
colpaerte <eli.colpaert@delaware.pro>
2019-09-03 14:01:14 +0200

    basic authentication

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,6 @@
 const path = require(`path`)
 const { createFilePath } = require(`gatsby-source-filesystem`)
-const { fmImagesToRelative } = require('gatsby-remark-relative-images');
-
+const { fmImagesToRelative } = require("gatsby-remark-relative-images")
 
 exports.createPages = ({ graphql, actions }) => {
   const { createPage } = actions
@@ -56,7 +55,7 @@ exports.createPages = ({ graphql, actions }) => {
 
 exports.onCreateNode = ({ node, actions, getNode }) => {
   const { createNodeField } = actions
-  fmImagesToRelative(node);
+  fmImagesToRelative(node)
 
   if (node.internal.type === `MarkdownRemark`) {
     const value = createFilePath({ node, getNode })
@@ -65,5 +64,40 @@ exports.onCreateNode = ({ node, actions, getNode }) => {
       node,
       value,
     })
+  }
+}
+
+exports.onCreatePage = async ({ page, actions }) => {
+  const { createPage } = actions
+
+  // page.matchPath is a special key that's used for matching pages
+  // only on the client.
+  if (page.path.match(/^\/account/)) {
+    page.matchPath = "/account/*"
+
+    // Update the page.
+    createPage(page)
+  }
+
+  exports.onCreateWebpackConfig = ({ stage, loaders, actions }) => {
+    if (stage === "build-html") {
+      /*
+       * During the build step, `auth0-js` will break because it relies on
+       * browser-specific APIs. Fortunately, we don’t need it during the build.
+       * Using Webpack’s null loader, we’re able to effectively ignore `auth0-js`
+       * during the build. (See `src/utils/auth.js` to see how we prevent this
+       * from breaking the app.)
+       */
+      actions.setWebpackConfig({
+        module: {
+          rules: [
+            {
+              test: /auth0-js/,
+              use: loaders.null(),
+            },
+          ],
+        },
+      })
+    }
   }
 }
