9dd9891f18aaf80b9e3c50dfce7f321f97ec42c8
colpaerte <eli.colpaert@delaware.pro>
2019-09-27 11:52:01 +0200

    Silent auth working, no gmail

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,12 +1,42 @@
-import "./src/css/style.css";
+import "./src/css/style.css"
+import React from "react"
+import { silentAuth } from "./src/utils/auth"
 // ES5 way
 // exports.onClientEntry = () => {
 // ES6 way
 export const onClientEntry = () => {
-    // IntersectionObserver polyfill for gatsby-background-image (Safari, IE)
-    if (typeof window.IntersectionObserver === `undefined`) {
-        import(`intersection-observer`)
-        console.log(`# IntersectionObserver is polyfilled!`)
-    }
+  // IntersectionObserver polyfill for gatsby-background-image (Safari, IE)
+  if (typeof window.IntersectionObserver === `undefined`) {
+    import(`intersection-observer`)
+    console.log(`# IntersectionObserver is polyfilled!`)
+  }
 }
 
+class SessionCheck extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      loading: true,
+    }
+  }
+
+  handleCheckSession = () => {
+    this.setState({ loading: false })
+  }
+
+  componentDidMount() {
+    silentAuth(this.handleCheckSession)
+  }
+
+  render() {
+    return (
+      this.state.loading === false && (
+        <React.Fragment>{this.props.children}</React.Fragment>
+      )
+    )
+  }
+}
+
+export const wrapRootElement = ({ element }) => {
+  return <SessionCheck>{element}</SessionCheck>
+}
