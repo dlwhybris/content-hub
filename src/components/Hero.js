43f06bb22ceef52cd89202e74906d52ed65e061e
colpaerte <eli.colpaert@delaware.pro>
2019-11-27 15:31:38 +0100

    fix intro text on hero

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -6,11 +6,14 @@ const Hero = ({ hero }) => {
     <div className="bg-gray-100 relative">
       <Img fluid={hero.coverImage.fluid} className="object-cover w-full h-96" />
       <p
-        className="absolute top-1/2 left-1/2 text-white text-2xl font-bold tracking-wide rotate-180 bg-black opacity-75 p-16"
+        className="invisible lg:visible absolute top-1/2 left-1/2 text-white text-2xl font-bold tracking-wide rotate-180 bg-black opacity-75 p-4 xl:p-16"
         dangerouslySetInnerHTML={{
           __html: hero.textOnCover.childMarkdownRemark.html,
         }}
       ></p>
+      <p className="visible lg:invisible absolute top-1/2 left-1/2 text-white sm:text-2xl text-xl font-bold tracking-wide rotate-180 bg-black opacity-75 sm:p-8 p-6">
+        {hero.textOnCover.childMarkdownRemark.excerpt}
+      </p>
     </div>
   )
 }
