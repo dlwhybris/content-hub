959620b026062410e1ff0914935c9c321f4f8d2b
colpaerte <eli.colpaert@delaware.pro>
2019-12-30 18:17:44 +0100

    case error

--- src/pages/auth0_callback.js
+++ src/pages/auth0_callback.js
@@ -1,8 +1,7 @@
 /* eslint-disable */
 import React, { useEffect } from "react"
-
 import { useAuth } from "react-use-auth"
-import Layout from "../components/layout"
+import Layout from "../components/Layout"
 
 const Auth0CallbackPage = () => {
   const { handleAuthentication } = useAuth()
