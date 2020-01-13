4e477a34e52c65ee0c9c95d7dfb9bc6d5e9ac45f
colpaerte <eli.colpaert@delaware.pro>
2020-01-13 12:22:27 +0100

    Pass correct paramter

--- src/pages/search.js
+++ src/pages/search.js
@@ -8,10 +8,15 @@ const SearchPage = ({ location }) => {
   const { siteUrl } = useSiteMetadata()
 
   return (
-    <Layout location={siteUrl} title={location.state.refineString}>
+    <Layout
+      location={siteUrl}
+      title={location.state ? location.state.searchString : "Search"}
+    >
       <main className="py-12 mx-auto max-w-md lg:max-w-4xl xl:max-w-6xl min-h-screen">
         <div className="p-20 bg-white  shadow-lg  rounded-lgflex flex-col justify-center">
-          <Search refineString={location.state.searchString} />
+          <Search
+            refineString={location.state ? location.state.searchString : ""}
+          />
         </div>
       </main>
     </Layout>
