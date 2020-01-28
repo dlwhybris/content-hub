c66e5192f4f3eb581ee14b3a8b08a9e0adb2d291
colpaerte <eli.colpaert@delaware.pro>
2020-01-28 17:17:58 +0100

    update Auth0 credentials

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -6,8 +6,8 @@ import { AuthProvider } from "react-use-auth"
 export const wrapRootElement = ({ element }) => (
   <AuthProvider
     navigate={navigate}
-    auth0_domain="content-hub-test.eu.auth0.com"
-    auth0_client_id="gQvhh7pm0ijyx3hFACXvrT26nXvPRc26"
+    auth0_domain={process.env.GATSBY_AUTH0_DOMAIN}
+    auth0_client_id={process.env.GATSBY_AUTH0_CLIENTID}
   >
     {element}
   </AuthProvider>
